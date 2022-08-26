ad3285f7b69ece82d2c91be27dfc2a85137cd3ab
Manish Sharma <manishsharma84848@gmail.com>
2022-08-26 17:47:23 +0530

    working on data.dart and aman.dart

---
+++ lib/aman.dart
@@ -0,0 +1,8 @@
+kncjhdsiuovjkfdjhvufdn cmkhbdsckvhgisdyhcx bnjklfhgdcbnjhfduvcnmb
+vdsvydgsycgvdsk
+    dv fdnsvbfdjbvjbjkds
+    vfdnsvjfdjvswdbjv dsb
+    bchdsvhvhjcvhjdscvhjds
+vdsvchdsbhvchvsdhvjchds
+    vdsvhjbsdhjbjcds
+vbdshvhjvsd
