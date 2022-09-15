286d89cfb1caacda727baeb79ab6265f834d12a7
Manish Sharma <manishsharma84848@gmail.com>
2022-09-15 18:51:47 +0530

    set theme or declare color

--- lib/my_theme/my_themedata.dart
+++ lib/my_theme/my_themedata.dart
@@ -34,23 +34,5 @@ class MyThemeData {
       textTheme: MyTextTheme().myTextTheme,
       textSelectionTheme: TextSelectionThemeData(cursorColor: MyColors().primary)
         );
-      primaryColor: MyColors().primary,
-      backgroundColor: MyColors().backGround,
-      textTheme: MyTextTheme().myTextTheme,
-      inputDecorationTheme: InputDecorationTheme(
-        contentPadding: const EdgeInsets.only(top: 20),
-        enabledBorder: UnderlineInputBorder(
-          borderSide: BorderSide(width: 2, color: MyColors().onPrimary),
-        ),
-        focusedBorder: UnderlineInputBorder(
-          borderSide: BorderSide(width: 2, color: MyColors().primary),
-        ),
-        errorBorder: UnderlineInputBorder(
-          borderSide: BorderSide(width: 2, color: MyColors().error),
-        ),focusedErrorBorder: UnderlineInputBorder(
-          borderSide: BorderSide(width: 2, color: MyColors().error),
-        ),
-      ),
-    );
   }
 }
