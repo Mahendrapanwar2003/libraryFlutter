7e6db3669160929825261d585a4615bc61d8f2b8
Mahendra Panwar <mahendrapanwar269@gmail.com>
2022-09-15 18:47:05 +0530

    textField themData

--- lib/my_theme/my_themedata.dart
+++ lib/my_theme/my_themedata.dart
@@ -3,9 +3,23 @@ part of ui_library;
 class MyThemeData {
   static ThemeData get themeData {
     return ThemeData(
-        primaryColor: MyColors().primary,
-        backgroundColor: MyColors().backGround,
-      textTheme: MyTextTheme().myTextTheme
-        );
+      primaryColor: MyColors().primary,
+      backgroundColor: MyColors().backGround,
+      textTheme: MyTextTheme().myTextTheme,
+      inputDecorationTheme: InputDecorationTheme(
+        contentPadding: const EdgeInsets.only(top: 20),
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(width: 2, color: MyColors().onPrimary),
+        ),
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(width: 2, color: MyColors().primary),
+        ),
+        errorBorder: UnderlineInputBorder(
+          borderSide: BorderSide(width: 2, color: MyColors().error),
+        ),focusedErrorBorder: UnderlineInputBorder(
+          borderSide: BorderSide(width: 2, color: MyColors().error),
+        ),
+      ),
+    );
   }
 }
