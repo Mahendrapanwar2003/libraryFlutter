e8840ddca2d6a66a8d65d9f80bdea6758bdcdc57
Manish Sharma <manishsharma84848@gmail.com>
2022-09-15 19:07:19 +0530

    set all

--- lib/my_theme/my_themedata.dart
+++ lib/my_theme/my_themedata.dart
@@ -1,38 +1,36 @@
 part of ui_library;
-
+//TODO Manish
 class MyThemeData {
   static ThemeData get themeData {
     return ThemeData(
-        primaryColor: MyColors().primary,
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-              minimumSize:Size(80.w, 50.px)
+      primaryColor: MyColors().primary,
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            minimumSize: Size(80.w, 50.px)),
+      ),
+      textButtonTheme: TextButtonThemeData(
+        style: TextButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
           ),
+          /*side: BorderSide(width: 2,color:MyColors().onPrimary),*/
+          /* minimumSize:Size(40.w, 40.px)*/
         ),
-        textButtonTheme: TextButtonThemeData(
-          style: TextButton.styleFrom(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-              /*side: BorderSide(width: 2,color:MyColors().onPrimary),*/
-             /* minimumSize:Size(40.w, 40.px)*/
-          )
+      ),
+      outlinedButtonTheme: OutlinedButtonThemeData(
+        style: OutlinedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+          side: BorderSide(width: 2, color: MyColors().onPrimary),
+          minimumSize: Size(40.w, 40.px),
         ),
-        outlinedButtonTheme: OutlinedButtonThemeData(
-          style: OutlinedButton.styleFrom(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-              side: BorderSide(width: 2,color:MyColors().onPrimary),
-              minimumSize:Size(40.w, 40.px)
-          )
-        ),
-        backgroundColor: MyColors().backGround,
+      ),
+      backgroundColor: MyColors().backGround,
       textTheme: MyTextTheme().myTextTheme,
-      textSelectionTheme: TextSelectionThemeData(cursorColor: MyColors().primary)
-        );
+    );
   }
 }
