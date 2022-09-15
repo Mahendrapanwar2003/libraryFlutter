6a488e0eeb6af4fef79abaf23e5ab6c2f532605b
HP <aman.dollop@gmail.com>
2022-09-15 18:35:24 +0530

    ButtonsTheme

--- lib/my_theme/my_themedata.dart
+++ lib/my_theme/my_themedata.dart
@@ -4,6 +4,32 @@ class MyThemeData {
   static ThemeData get themeData {
     return ThemeData(
         primaryColor: MyColors().primary,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+              minimumSize:Size(80.w, 50.px)
+          ),
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+              /*side: BorderSide(width: 2,color:MyColors().onPrimary),*/
+             /* minimumSize:Size(40.w, 40.px)*/
+          )
+        ),
+        outlinedButtonTheme: OutlinedButtonThemeData(
+          style: OutlinedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+              side: BorderSide(width: 2,color:MyColors().onPrimary),
+              minimumSize:Size(40.w, 40.px)
+          )
+        ),
         backgroundColor: MyColors().backGround,
       textTheme: MyTextTheme().myTextTheme
         );
