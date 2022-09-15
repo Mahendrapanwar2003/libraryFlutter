ab6cb0338a4fcf9b999e878d2303c49c27c175f8
Manish Sharma <manishsharma84848@gmail.com>
2022-09-15 19:24:00 +0530

    Update by mahendra

--- lib/my_theme/my_themedata.dart
+++ lib/my_theme/my_themedata.dart
@@ -3,9 +3,26 @@ part of ui_library;
 class MyThemeData {
   static ThemeData get themeData {
     return ThemeData(
-        primaryColor: MyColors().primary,
-        backgroundColor: MyColors().backGround,
-        textTheme: MyTextTheme().myTextTheme
+      primaryColor: MyColors().primary,
+      backgroundColor: MyColors().backGround,
+      textTheme: MyTextTheme().myTextTheme,
+      textSelectionTheme:
+          TextSelectionThemeData(cursorColor: MyColors().primary),
+      inputDecorationTheme: InputDecorationTheme(
+        contentPadding: const EdgeInsets.only(top: 20),
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(width: 2, color: MyColors().onPrimary),
+        ),
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(width: 2, color: MyColors().primary),
+        ),
+        errorBorder: UnderlineInputBorder(
+          borderSide: BorderSide(width: 2, color: MyColors().error),
+        ),
+        focusedErrorBorder: UnderlineInputBorder(
+          borderSide: BorderSide(width: 2, color: MyColors().error),
+        ),
+      ),
     );
   }
 }
