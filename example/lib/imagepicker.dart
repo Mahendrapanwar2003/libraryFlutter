98fa409806b082b423bfdeca712b0499c2b4b581
Mahendra Panwar <mahendrapanwar269@gmail.com>
2022-09-16 19:48:00 +0530

    working background

--- example/lib/imagepicker.dart
+++ example/lib/imagepicker.dart
@@ -1,11 +1,7 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:ui_library/ui_library.dart';
 
 class ImageVi extends StatefulWidget {
-  File? path;
-
   ImageVi({Key? key}) : super(key: key);
 
   @override
@@ -13,34 +9,26 @@ class ImageVi extends StatefulWidget {
 }
 
 class _ImageViState extends State<ImageVi> {
+
   @override
   Widget build(BuildContext context) {
-    print("path:-1${widget.path?.path}");
-    return Container(
-      height: 100,
-      width: 100,
-      decoration: BoxDecoration(
-          color: Colors.red,
-          image: DecorationImage(
-            image: (widget.path != null
-                    ? FileImage(widget.path!)
-                    : const NetworkImage(
-                        "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg"))
-                as ImageProvider,
-          )),
-      child: Column(
+    return Scaffold(
+      body: Stack(
         children: [
-          ElevatedButton(
-            child: const Text("pickImage"),
-            onPressed: () async {
-              File? file = await MyImagePicker.pickImage(context: context);
+          SizedBox(
+            height: double.infinity,
+            width: double.infinity,
+            child: Stack(
+              children: [
+                const SizedBox(
+                  height: 400,
+                  child: HeaderWidget(
+                    height: 400,
+                  ),
+                ),
 
-              setState(
-                () async {
-                  if (widget.path != null) widget.path = file;
-                },
-              );
-            },
+              ],
+            ),
           ),
         ],
       ),
