5094e9a74f5a981415d8cf938265c3dbc259e984
HP <aman.dollop@gmail.com>
2022-09-16 19:36:38 +0530

    MythemeData

--- lib/my_theme/my_themedata.dart
+++ lib/my_theme/my_themedata.dart
@@ -7,7 +7,7 @@ class MyThemeData {
       backgroundColor: MyColors().backGround,
       textTheme: MyTextTheme().myTextTheme,
       textSelectionTheme:
-          TextSelectionThemeData(cursorColor: MyColors().primary),
+      TextSelectionThemeData(cursorColor: MyColors().primary),
       inputDecorationTheme: InputDecorationTheme(
         contentPadding: const EdgeInsets.only(top: 20),
         enabledBorder: UnderlineInputBorder(
@@ -25,19 +25,20 @@ class MyThemeData {
       ),
       elevatedButtonTheme: ElevatedButtonThemeData(
         style: ElevatedButton.styleFrom(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30),
-            ),
-            minimumSize: Size(80.w, 50.px)),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(30),
+          ),
+          
+          minimumSize: Size(80.w, 50.px),padding: EdgeInsets.all(1)),
       ),
       textButtonTheme: TextButtonThemeData(
           style: TextButton.styleFrom(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30),
-        ),
-        /*side: BorderSide(width: 2,color:MyColors().onPrimary),*/
-        /* minimumSize:Size(40.w, 40.px)*/
-      )),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            /*side: BorderSide(width: 2,color:MyColors().onPrimary),*/
+            /* minimumSize:Size(40.w, 40.px)*/
+          )),
       outlinedButtonTheme: OutlinedButtonThemeData(
           style: OutlinedButton.styleFrom(
               shape: RoundedRectangleBorder(
