62027ffec31972b1453996c135f2de25180f2b88
HP <aman.dollop@gmail.com>
2022-10-06 13:05:17 +0530

    Change in MyThemeData

--- lib/my_http/my_http.dart
+++ lib/my_http/my_http.dart
@@ -12,28 +12,26 @@ class MyHttp {
     return response;
   }
 
-  static Future<http.Response> postMethod(
-      {required String url,
-      required Map<String, dynamic> bodyParams,
-      Map<String, String>? token}) async {
+  static Future<http.Response> postMethod({required String url,
+    required Map<String, dynamic> bodyParams,
+    Map<String, String>? token}) async {
     if (kDebugMode) print("CALLING:: $url");
     http.Response response =
-        await http.post(Uri.parse(url), body: bodyParams, headers: token);
+    await http.post(Uri.parse(url), body: bodyParams, headers: token);
     if (kDebugMode) print("CALLING:: ${response.body}");
     return response;
   }
 
-  static Future<http.Response> multipartRequest(
-      {File? image,
-      required String url,
-      required String multipartRequestType /* POST or GET */,
-      required Map<String, dynamic> bodyParams,
-      required String token,
-      required String userProfileImageKey}) async {
+  static Future<http.Response> multipartRequest({File? image,
+    required String url,
+    required String multipartRequestType /* POST or GET */,
+    required Map<String, dynamic> bodyParams,
+    required String token,
+    required String userProfileImageKey}) async {
     http.Response res;
     if (image != null) {
       http.MultipartRequest multipartRequest =
-          http.MultipartRequest(multipartRequestType, Uri.parse(url));
+      http.MultipartRequest(multipartRequestType, Uri.parse(url));
       bodyParams.forEach((key, value) {
         multipartRequest.fields[key] = value;
       });
