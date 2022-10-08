0739d3c740177c9a49a8c684ed7d7ef3b2e76e1a
Manish Sharma <manishsharma84848@gmail.com>
2022-10-08 19:28:41 +0530

    minor fix

--- lib/my_http/my_http.dart
+++ lib/my_http/my_http.dart
@@ -62,9 +62,9 @@ class MyHttp {
 
   static Future<http.Response> getMethodForParams(
       {required Map<String, String> authorization,
-      required Map<String, dynamic> queryParameters,
-      required String baseUri,
-      required String endPointUri}) async {
+        required Map<String, dynamic> queryParameters,
+        required String baseUri,
+        required String endPointUri}) async {
     Uri uri = Uri.http(baseUri, endPointUri, queryParameters);
     if (kDebugMode) print("CALLING:: $uri");
     http.Response response = await http.get(uri, headers: authorization);
